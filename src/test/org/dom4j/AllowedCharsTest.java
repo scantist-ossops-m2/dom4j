653b8ea856759ddc31a402f793adcd1f1ac54d38
anmarker <anmarker@cisco.com>
2018-11-26 10:18:36 +0100

    added JUnit 4 for AllowedCharsTest

--- src/test/org/dom4j/AllowedCharsTest.java
+++ src/test/org/dom4j/AllowedCharsTest.java
@@ -1,6 +1,6 @@
 package org.dom4j;
 
-import org.testng.annotations.Test;
+import org.junit.Test;
 
 /**
  * @author Filip Jirsák
@@ -13,7 +13,7 @@ public class AllowedCharsTest {
         QName.get("elem:ent");
     }
 
-    @Test(expectedExceptions = IllegalArgumentException.class)
+    @Test(expected = IllegalArgumentException.class)
     public void localNameFail() {
         QName.get("!element");
     }
@@ -24,50 +24,50 @@ public class AllowedCharsTest {
         QName.get("ns:element", "http://example.com/namespace");
     }
 
-    @Test(expectedExceptions = IllegalArgumentException.class)
+    @Test(expected = IllegalArgumentException.class)
     public void qnameFail1() {
         QName.get("ns:elem:ent", "http://example.com/namespace");
     }
 
-    @Test(expectedExceptions = IllegalArgumentException.class)
+    @Test(expected = IllegalArgumentException.class)
     public void qnameFail2() {
         QName.get(":nselement", "http://example.com/namespace");
     }
 
-    @Test(expectedExceptions = IllegalArgumentException.class)
+    @Test(expected = IllegalArgumentException.class)
     public void createElementLT() {
         DocumentHelper.createElement("element<name");
     }
 
-    @Test(expectedExceptions = IllegalArgumentException.class)
+    @Test(expected = IllegalArgumentException.class)
     public void createElementGT() {
         DocumentHelper.createElement("element>name");
     }
 
-    @Test(expectedExceptions = IllegalArgumentException.class)
+    @Test(expected = IllegalArgumentException.class)
     public void createElementAmpersand() {
         DocumentHelper.createElement("element&name");
     }
 
-    @Test(expectedExceptions = IllegalArgumentException.class)
+    @Test(expected = IllegalArgumentException.class)
     public void addElement() {
         Element root = DocumentHelper.createElement("root");
         root.addElement("element>name");
     }
 
-    @Test(expectedExceptions = IllegalArgumentException.class)
+    @Test(expected = IllegalArgumentException.class)
     public void addElementQualified() {
         Element root = DocumentHelper.createElement("root");
         root.addElement("element>name", "http://example.com/namespace");
     }
 
-    @Test(expectedExceptions = IllegalArgumentException.class)
+    @Test(expected = IllegalArgumentException.class)
     public void addElementQualifiedPrefix() {
         Element root = DocumentHelper.createElement("root");
         root.addElement("ns:element>name", "http://example.com/namespace");
     }
 
-    @Test(expectedExceptions = IllegalArgumentException.class)
+    @Test(expected = IllegalArgumentException.class)
     public void addElementPrefix() {
         Element root = DocumentHelper.createElement("root");
         root.addElement("ns>:element", "http://example.com/namespace");
